12f8b39f5b317306ae8a71835817d084acebaf64
magnusmartin85 <martin@magnusmartin.com>
2020-03-13 20:03:54 +0100

    FEAT: add gulp for image optimization.

--- gulpfile.js
+++ gulpfile.js
@@ -1,72 +1,20 @@
-const babel = require('gulp-babel');
-const concat = require('gulp-concat');
 const gulp = require('gulp');
-const imagemin = require('gulp-imagemin');
 const imageResize = require('gulp-image-resize');
-const imgDest = 'dist/img/';
-const imgSrc = 'src/img/**/*.{png,jpg}';
-const sass = require('gulp-sass');
-const uglify = require('gulp-uglifyjs');
-
-sass.compiler = require('node-sass');
-
-gulp.task('styles', () => {
-    return gulp.src('./src/scss/index.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./dist/css'));
-});
-
-gulp.task('styles:watch', () => {
-    gulp.watch('./src/scss/index.scss', gulp.parallel('styles'));
-});
-
-gulp.task('scripts', () => {
-    return gulp.src([
-        'src/js/jquery-3.4.1.min.js',
-        'src/js/lightbox.js'
-    ])
-        .pipe(babel({
-            presets: ['@babel/env']
-        }))
-        .pipe(concat('app.js'))
-        .pipe(gulp.dest('dist/js'));
-});
-
-gulp.task('uglify', done => {
-    gulp.src('dist/js/app.js')
-        .pipe(uglify('app.min.js'))
-        .pipe(gulp.dest('dist/js'));
-    done();
-});
-
-gulp.task('copyHtml', () => {
-    return gulp.src('src/index.html')
-        .pipe(gulp.dest('dist'))
-});
-
-gulp.task('imagemin', done => {
-    gulp.src(imgSrc)
-        .pipe(imagemin())
-        .pipe(gulp.dest(imgDest));
-    done();
-});
+const imgSrc = 'src/images/**/*.{png,jpg}';
+const imgDest = 'dist/images/';
 
 gulp.task('optimizeImages', done => {
     gulp.src(imgSrc)
         .pipe(imageResize({
-            width: 1000,
-            height: 1000,
+            width: 1200,
+            height: 1200,
             imageMagick: true
         }))
-        .pipe(imagemin())
         .pipe(gulp.dest(imgDest));
     done();
 });
 
-gulp.task('default', gulp.parallel(
-    'scripts',
-    'styles',
-    'optimizeImages',
-    'copyHtml'
+gulp.task('default', gulp.series(
+    'optimizeImages'
     )
 );

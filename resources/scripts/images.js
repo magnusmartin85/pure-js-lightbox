b159565c098a920bbb621ae364c7feb67ea98098
magnusmartin85 <martin@magnusmartin.com>
2021-10-14 18:15:17 +0200

    FEAT: move image file.

---
+++ resources/scripts/images.js
@@ -0,0 +1,38 @@
+export default [
+  {
+    description: 'Body of Water Between Green Leaf Trees',
+    imagePath: './images/01.jpg',
+    id: 0,
+    source: 'Photo by Ian Turnell from Pexels',
+  },
+  {
+    description: 'High Angle-photography of Green Forest Trees',
+    imagePath: './images/02.jpg',
+    id: 1,
+    source: 'Photo by Jaymantri from Pexels',
+  },
+  {
+    description: 'Trees Under Blue Sky during Daytime',
+    imagePath: './images/03.jpg',
+    id: 2,
+    source: 'Photo by Markus Spiske temporausch.com from Pexels',
+  },
+  {
+    description: 'Moraine Lake Sunrise',
+    imagePath: './images/04.jpg',
+    id: 3,
+    source: 'Photo by James Wheeler from Pexels',
+  },
+  {
+    description: 'Green Trees Under Blue and Orange Sky during Sunset',
+    imagePath: './images/05.jpg',
+    id: 4,
+    source: 'Photo by Joyston Judah from Pexels',
+  },
+  {
+    description: 'Multicolored Hot Air Balloon over Calm Sea',
+    imagePath: './images/06.jpg',
+    id: 5,
+    source: 'Photo by Bess Hamiti from Pexels',
+  }
+];

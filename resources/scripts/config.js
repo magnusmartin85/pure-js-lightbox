239475feade2dd223958dd069c5fc7b24c1c7a7b
magnusmartin85 <martin@magnusmartin.com>
2021-10-14 18:14:02 +0200

    FEAT: add config file.

---
+++ resources/scripts/config.js
@@ -0,0 +1,16 @@
+import images from './images';
+
+export default {
+  images,
+  imageSlider: {
+    showImageSource: true,
+    showImageTitle: true,
+    showImageNumbers: true
+  },
+  previewImage: {
+    overlay: {
+      showImageTitle: true,
+      showImageSource: true
+    }
+  }
+}

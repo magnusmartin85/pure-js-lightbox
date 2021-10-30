5b2700cf167662b08a594961347b7745f04f80ec
magnusmartin85 <martin@magnusmartin.com>
2021-10-30 15:40:12 +0200

    FEAT: update gulpfile.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -6,26 +6,29 @@ import autoprefixer from 'gulp-autoprefixer';
 import babel from 'gulp-babel';
 import browserSync from 'browser-sync';
 import concat from 'gulp-concat';
+import dartSass from 'sass'
 import del from 'del';
 import dependents from 'gulp-dependents';
+import gulpSass from 'gulp-sass'
 import imagemin from 'gulp-imagemin';
 import imageResize from 'gulp-image-resize';
 import minifyCss from 'gulp-clean-css';
 import plumber from 'gulp-plumber';
 import rename from 'gulp-rename';
 import sourcemaps from 'gulp-sourcemaps';
+import svgmin from 'gulp-svgmin';
 import uglify from 'gulp-uglify';
 import webpack from 'webpack-stream';
+import {dependencies} from './package.json';
 
-
-const sass = require('gulp-sass')(require('sass'));
-const node_dependencies = Object.keys(require('./package.json').dependencies || {});
+const sass = gulpSass(dartSass);
+const node_dependencies = Object.keys(dependencies);
 
 // directories
 const dist_folder = './dist/';
-const node_modules_folder = './node_modules/';
 const dist_node_modules_folder = dist_folder + 'node_modules/';
-const src_folder = './resources/';
+const node_modules_folder = './node_modules/';
+const src_folder = './src/';
 
 
 gulp.task('clear', () => del([dist_folder]));
@@ -38,9 +41,9 @@ gulp.task('html', () => {
     .pipe(browserSync.stream());
 });
 
-gulp.task('sass', () => {
+gulp.task('scss', () => {
   return gulp.src([
-    src_folder + 'styles/lightbox.scss'
+    src_folder + 'scss/lightbox.scss'
   ])
     .pipe(sourcemaps.init())
     .pipe(plumber())
@@ -57,8 +60,8 @@ gulp.task('sass', () => {
     .pipe(browserSync.stream());
 });
 
-gulp.task('images', () => {
-  return gulp.src([src_folder + 'images/**/*.jpg'], {since: gulp.lastRun('images')})
+gulp.task('img', () => {
+  return gulp.src([src_folder + 'img/**/*.jpg'], {since: gulp.lastRun('img')})
     .pipe(plumber())
     .pipe(imageResize({
       width: 1000,
@@ -67,7 +70,7 @@ gulp.task('images', () => {
       upscale: false
     }))
     .pipe(imagemin())
-    .pipe(gulp.dest(dist_folder + 'images'))
+    .pipe(gulp.dest(dist_folder + 'img'))
     .pipe(imageResize({
       width: 300,
       height: 300,
@@ -75,15 +78,23 @@ gulp.task('images', () => {
       upscale: false
     }))
     .pipe(imagemin())
-    .pipe(gulp.dest(dist_folder + 'images/preview'))
+    .pipe(gulp.dest(dist_folder + 'img/preview'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('svg', () => {
+  return gulp.src([src_folder + '**/*.svg'], {since: gulp.lastRun('svg')})
+    .pipe(plumber())
+    .pipe(svgmin())
+    .pipe(gulp.dest(dist_folder))
     .pipe(browserSync.stream());
 });
 
 gulp.task('js', () => {
-  return gulp.src([src_folder + 'scripts/**/*'])
+  return gulp.src([src_folder + 'js/**/*'])
     .pipe(plumber())
     .pipe(webpack({
-      mode: 'development',    // change to 'production' for minified build
+      mode: 'production',    // change to 'production' for minified build
     }))
     .pipe(sourcemaps.init())
     .pipe(babel({
@@ -115,9 +126,9 @@ gulp.task('vendor', () => {
     .pipe(browserSync.stream());
 });
 
-gulp.task('build', gulp.series('clear', 'html', 'sass', 'js', 'vendor', 'images'));
+gulp.task('build', gulp.series('clear', 'html', 'scss', 'js', 'vendor', 'img', 'svg'));
 
-gulp.task('dev', gulp.series('html', 'sass', 'js', 'images'));
+gulp.task('dev', gulp.series('html', 'scss', 'js', 'img', 'svg'));
 
 gulp.task('serve', () => {
   return browserSync.init({
@@ -138,9 +149,9 @@ gulp.task('watch', () => {
 
   const watch = [
     src_folder + '**/*.html',
-    src_folder + 'styles/**/*.scss',
-    src_folder + 'scripts/**/*.js',
-    src_folder + 'images/**/*.jpg'
+    src_folder + 'scss/**/*.scss',
+    src_folder + 'js/**/*.js',
+    src_folder + 'img/**/*.jpg'
   ];
 
   gulp.watch(watch, gulp.series('dev')).on('change', browserSync.reload);
